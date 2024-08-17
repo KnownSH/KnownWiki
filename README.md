9f9c5c479e34ff19c8e5c5c23146cfafe381fed7
KnownSH <xanp101@gmail.com>
2024-08-16 23:56:43 -0700

    Import all

---
+++ README.md
@@ -0,0 +1,1 @@
+# KnownWiki Source
---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+import expressiveCode from "astro-expressive-code";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    expressiveCode(), 
+    tailwind(), 
+    mdx(), 
+    react()
+  ]
+});
